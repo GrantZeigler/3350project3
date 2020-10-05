5a77770c5639245d9b2617d1f9468191d15df8ee
Grant David Zeigler <gzeigle@bgsu.edu>
2020-10-05 15:44:40 -0400

    2

--- poly.cpp
+++ poly.cpp
@@ -1,11 +1,32 @@
 //poly.cpp
-
+//Grant Zeigler
+#include <cassert>
+#include <iostream>
 #include "poly.h"
+
+using namespace std;
 
 namespace main_savitch_5
 {
+	void poly::copy(const poly& p)
+	{
+		// Pre: p is a valid polynomial.
+		// Post: p is copied into the implicit parameter.
 
+		nterms = p.nterms;
+		terms = new term[MAXSIZE = p.MAXSIZE];
+		for (unsigned int i = 0; i < nterms; i++)
+			terms[i] = p.terms[i];
+	};
 
+	poly::poly(const poly& p) //DEEP COPY SEMANTICS
+	{
+		// Pre: p is a valid polynomial.
+		// Post: p is copied into the implicit parameter
+		// using "deep copy semantics."
+
+		copy(p);
+	};
 
 
 
